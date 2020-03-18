7e7d69319cd0491f4894e54413b8c08c52292a56
jod25 <jod25@uclive.ac.nz>
2020-03-18 19:26:28 +1300

    added error handling for no name given for POST /users/register

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -6,12 +6,13 @@ exports.register = async function (req, res) {
 
     // If the email does not contain '@' return status 400
     try {
+        if (req.body.name === undefined) throw "ERROR no name given";
         if (!req.body.email.includes('@')) throw "ERROR email invalid";
         const userId = await users.registerUser(req.body);
         res.status(201)
             .send({"userId": userId})
     } catch (err) {
-        if (err == "ERROR email invalid") {
+        if (err == "ERROR email invalid" || err == "ERROR no name given") {
             res.status(400)
                 .send(err);
         }
