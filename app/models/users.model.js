53d7eb643c34a8d757cbc22e95b583a5b62fb2fb
jod25 <jod25@uclive.ac.nz>
2020-03-19 15:14:26 +1300

    Added password hashing

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,21 +1,25 @@
 const db = require('../../config/db');
+const passwords = require('bcrypt');
+
 
 exports.registerUser = async function (user_data) {
     const conn = await db.getPool().getConnection();
     const query = 'INSERT INTO User (name, email, password, city, country) ' +
                   'VALUES (?, ?, ?, ?, ?)';
-    const [result] = await conn.query(query, [user_data.name, user_data.email, user_data.password, user_data.city, user_data.country]);
+    const hashed_password = await passwords.hash(user_data.password, 10);
+    const [result] = await conn.query(query, [user_data.name, user_data.email, hashed_password, user_data.city, user_data.country]);
     conn.release();
     return result.insertId;
 };
 
 exports.login = async function (user_data) {
     const conn = await db.getPool().getConnection();
-    const query = 'SELECT user_id FROM User WHERE email = ? AND password = ?';
-    const [result] = await conn.query(query, [user_data.email, user_data.password]);
+    const query = 'SELECT user_id, password FROM User WHERE email = ?';
+    const [result] = await conn.query(query, [user_data.email]);
     conn.release();
     var token = randomNum() + randomNum();
-    if (result[0] === undefined) {
+    password_correct = await passwords.compare(user_data.password, result[0].password);
+    if (result[0] === undefined || !password_correct) {
         return null;
     } else {
         return {"userId": result[0].user_id, "token": token};
